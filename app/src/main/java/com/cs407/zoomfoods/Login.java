99c614baaef515d77ca87ffe4ee6b37fff15c405
vazhan <145057549+vazhan@users.noreply.github.com>
2023-10-31 23:43:37 -0500

    Initial commit of Login.java [Vienna]

---
+++ app/src/main/java/com/cs407/zoomfoods/Login.java
@@ -0,0 +1,16 @@
+package com.cs407.zoomfoods;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cs407.cs407zoomfoods.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
