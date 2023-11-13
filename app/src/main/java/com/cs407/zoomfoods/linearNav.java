bb13d1f4c1f2166cc7da4ff9eb2da1b88b905702
sfisher <safisher3@wisc.edu>
2023-11-13 00:01:36 -0600

    basic linear nav fragment (not yet functional) [Shia]

---
+++ app/src/main/java/com/cs407/zoomfoods/linearNav.java
@@ -0,0 +1,21 @@
+package com.cs407.zoomfoods;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+class linearNav extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_linear_nav, container, false);
+    }
+}
